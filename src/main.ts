44052f853dff11848860ce75099bc73157415a91
youranreus <youranreus@qq.com>
2023-12-03 14:01:35 +0800

    feat: basic deps

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// 通用字体
+import 'vfonts/Lato.css'
+// 等宽字体
+import 'vfonts/FiraCode.css'
+
 const app = createApp(App)
 
 app.use(createPinia())
