952a61461ad07db25e2e6e3c5927c96441e8e889
youranreus <youranreus@qq.com>
2023-12-10 18:55:35 +0800

    feat: 临时首页

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main>haha</main>
+  <main class="h-[calc(100vh-119px)] flex items-center justify-center">
+    <n-result status="418" title="本来应该是一个看板" description="但是还没做"></n-result>
+  </main>
 </template>
