3048e0e65485a44056a67474589ed9560f230d80
youranreus <youranreus@qq.com>
2023-12-16 17:46:16 +0800

    feat: 文案更新

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -29,6 +29,6 @@ onUnmounted(unwatch)
 
 <template>
   <main class="h-[calc(100vh-119px)] flex items-center justify-center">
-    <n-result status="418" title="本来应该是一个看板" description="但是还没做"></n-result>
+    <n-result status="418" title="本来应该是一个看板" description="但是还没做TAT"></n-result>
   </main>
 </template>
